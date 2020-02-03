743d5fc97ea84f55669491edc53cc6cbe105c99b
Phil Goodwin <phil.goodwin@gmail.com>
2020-02-03 11:20:26 -0800

    get tests to pass on command line

--- test/fixtures/fixture_reader.dart
+++ test/fixtures/fixture_reader.dart
@@ -1,5 +1,9 @@
 import 'dart:convert';
 import 'dart:io';
+import "package:path/path.dart";
 
-Map<String, dynamic> jsonFromFixture(String name) =>
-    json.decode(File('test/fixtures/$name').readAsStringSync());
+Map<String, dynamic> jsonFromFixture(String name) {
+  var scriptPath = dirname(Platform.script.path).replaceAll('test','');
+  return json.decode(File(join(scriptPath, 'test/fixtures/$name'))
+        .readAsStringSync());
+}
