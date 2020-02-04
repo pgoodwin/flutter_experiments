68c3cd35bf02e4c0a961e47452bdb6c0a4f7ec7f
Phil Goodwin <phil.goodwin@gmail.com>
2020-02-04 13:17:11 -0800

    Make up some number trivia application states

--- lib/src/number_trivia/number_trivia_bloc/number_trivia_state.dart
+++ lib/src/number_trivia/number_trivia_bloc/number_trivia_state.dart
@@ -1,10 +1,25 @@
 import 'package:equatable/equatable.dart';
+import 'package:flutter_experiments/src/number_trivia/number_trivia.dart';
+import 'package:meta/meta.dart';
 
+@immutable
 abstract class NumberTriviaState extends Equatable {
-  const NumberTriviaState();
+  NumberTriviaState([List props = const <dynamic>[]]) : super(props);
 }
 
-class InitialNumberTriviaState extends NumberTriviaState {
-  @override
-  List<Object> get props => [];
+class Empty extends NumberTriviaState {}
+
+class Loading extends NumberTriviaState {}
+
+class Loaded extends NumberTriviaState {
+  final NumberTrivia trivia;
+
+  Loaded({@required this.trivia}) : super([trivia]);
 }
+
+class Error extends NumberTriviaState {
+  final String message;
+
+  Error({@required this.message}) : super([message]);
+}
+
